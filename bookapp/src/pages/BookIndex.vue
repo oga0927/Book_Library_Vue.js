39b4b3e0f09022eeac3775120dfc9aeda80ad848
oga-0927 <totti0927.oga@gmail.com>
2021-03-20 15:06:42 +0900

    v-cardで読んだ日と感想を表示

--- bookapp/src/pages/BookIndex.vue
+++ bookapp/src/pages/BookIndex.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>BookIndex</div>
+  <div>
+    <v-row>
+      <v-col cols="6">
+        <v-btn color="primary" to="/search">検索する</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" v-for="book in books" :key="book.id">
+        <v-card>
+          <v-row>
+            <v-col cols="4">
+              <!-- 画像が表示される -->
+              <v-img :src="book.image"></v-img>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="8">
+        <!-- <v-card-title>{{ book.title }}</v-card-title>
+        読んだ日：{{ book.readDate }}
+        感想：{{ book.memo }} -->
+        <v-spacer></v-spacer>
+
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    books: Array
+  }
 }
 </script>
 
