1bb900348a2dc680c9538f2e2c71b9a158bb3133
oga-0927 <totti0927.oga@gmail.com>
2021-03-20 10:26:51 +0900

    title_error

--- bookapp/src/pages/BookEdit.vue
+++ bookapp/src/pages/BookEdit.vue
@@ -1,5 +1,6 @@
 <template>
   <div>BookEdit
+    {{book.title}}
     {{ books }}
   </div>
 </template>
@@ -9,7 +10,21 @@ export default {
   name: 'BookEdit',
   props: {
     books:Array
-  }
+  },
+  data() {
+    return {
+      book: ''
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+  next(vm => {
+    // `vm` を通じてコンポーネントインスタンスにアクセス
+    vm.$nextTick(() => {
+      vm.book = vm.books[vm.$route.params.id]
+      console.log(vm.book);
+    })
+  })
+}
 }
 </script>
 
