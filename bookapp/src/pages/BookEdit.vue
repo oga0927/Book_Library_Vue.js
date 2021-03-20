5fbc5156554d88454e2d7b7ff3a35c4fbbf210ac
oga-0927 <totti0927.oga@gmail.com>
2021-03-20 12:02:30 +0900

    カレンダー機能実装

--- bookapp/src/pages/BookEdit.vue
+++ bookapp/src/pages/BookEdit.vue
@@ -1,7 +1,56 @@
 <template>
-  <div>BookEdit
-    {{book.title}}
-    {{ books }}
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <!-- book.imageでURLを取得する -->
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>
+                タイトル：{{ book.title }}
+              </v-card-title>
+                読んだ日：
+                <v-menu
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="date"
+                    @input="menu = false"
+                    locale="jp-ja"
+                    :day-format="date => new Date(date).getDate()"
+                  ></v-date-picker>
+                </v-menu>
+
+                感想：<v-textarea
+                class="mx-2" v-model="book.memo">
+                {{ book.memo }}
+                </v-textarea>
+                <v-card-actions>
+                  <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+                  <v-btn color="info">保存する</v-btn>
+                </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -13,7 +62,9 @@ export default {
   },
   data() {
     return {
-      book: ''
+      book: '',
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
     }
   },
   beforeRouteEnter (to, from, next) {
