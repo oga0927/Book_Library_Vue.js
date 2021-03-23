92ed1e88f36626e33fcae1a8650949e7c93057cc
oga-0927 <totti0927.oga@gmail.com>
2021-03-23 22:15:18 +0900

    新規ログインフォーム作成

--- bookapp/src/global/AppHeader.vue
+++ bookapp/src/global/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <span>
-    <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark>
+    <v-navigation-drawer app v-model="drawer" class="brown darken-4" dark>
       <v-list>
         <div v-if="!isAuthenticated">
           <v-btn text to="/sign-in" data-cy="signinBtn">ログイン</v-btn>
           <v-btn
-            color="brown lighten-3"
+            text
             to="/join"
             class="nav-join"
             data-cy="joinBtn"
@@ -20,7 +20,7 @@
         </div>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app color="primary darken-2" dark>
+    <v-app-bar app color="grey darken-4" dark>
       <v-icon
         class="hidden-md-and-up"
         @click="drawer = !drawer"
---
+++ bookapp/src/pages/Register.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-form>
+    <v-content>
+      <v-card width="500" class="mx-auto mt-5">
+        <v-card-title>ログインフォーム</v-card-title>
+        <v-card-text>
+          <v-text-field label="ユーザー名" prepend-icon="mdi-account-circle"/>
+          <v-text-field 
+            placeholder="パスワードは6文字以上必要です"
+            label="パスワード" 
+            required
+            :type="showPassword ? 'text' : 'password'" 
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"/>
+        </v-card-text>
+
+        <v-divider></v-divider>
+        <v-card-actions>
+          <!-- <v-btn color="success">ログイン</v-btn> -->
+          <v-btn color="info">ユーザー登録</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'LogUp',
+  data() {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
