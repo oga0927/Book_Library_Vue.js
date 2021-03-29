141124466325ebd94c1383fe8ea51369a02ac1ec
oga-0927 <totti0927.oga@gmail.com>
2021-03-29 16:56:42 +0900

    validate

--- bookapp/src/pages/Login.vue
+++ bookapp/src/pages/Login.vue
@@ -1,82 +1,84 @@
 <template>
-  <v-container fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar dark color="primary">
-              <v-toolbar-title>ログイン</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form ref="form" v-model="valid">
-            <v-text-field
-              name="email"
-              label="Email"
-              type="email"
-              v-model="email"
-              :rules="emailRules"
-              required
-              data-cy="loginEmailField"
-            >
-            </v-text-field>
-            <v-text-field
-              name="password"
-              label="Password"
-              type="password"
-              data-cy="loginPasswordField"
-              v-model="password"
-              :rules="passwordRules"
-              required
-            >
-            </v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="primary"
-              :disabled="!valid"
+  <v-form>
+    <v-main>
+      <v-card width="500" class="mx-auto mt-5">
+        <v-card-title>ログインフォーム</v-card-title>
+        <v-card-text>
+          
+          <v-text-field
+            name="email"
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            prepend-icon="mdi-email"
+            required
+            data-cy="loginEmailField" />
+
+          <v-text-field
+            name="password"
+            label="Password" 
+            :type="showPassword ? 'text' : 'password'" 
+            v-model="password"
+            :rules="passwordRules"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+            data-cy="loginPasswordField"
+            required/>
+        </v-card-text>
+
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn 
+              color="info"
               @click="submit"
-              data-cy="signinSubmitBtn"
-              >Login</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+            >ログイン</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-form>
 </template>
 
+
+
 <script>
+// import firebase from 'firebase'
 export default {
-    name: 'Signin',
-    data() {
-        return {
-            valid: false,
-            email: '',
-            password: '',
-            emailRules: [
-                v => !!v || 'E-mail is required',
-                v => /.+@.+/.test(v) || 'E-mail must be valid'
-            ],
-            passwordRules: [
-                v => !!v || 'Password is required',
-                v =>
-                    v.length >= 6 ||
-                    'Password must be greater than 6 characters'
-            ]
-        };
-    },
-    methods: {
-        submit() {
-            if (this.$refs.form.validate()) {
-                this.$store.dispatch('userLogin', {
-                    email: this.email,
-                    password: this.password
-                });
-            }
-        }
+  name: 'LogIn',
+  
+  data() {
+    return {
+      valid: false,
+      showPassword: false,
+      username: "",
+      email: "",
+      password: "",
+      emailRules: [
+        v => !!v || "メールアドレスを入力してください",
+        v => /.+@.+/.test(v) || "正しいメールアドレスを入力してください"
+      ],
+      passwordRules: [
+        v => !!v || "パスワードを入力してください",
+        v => v.length >= 6 || "パスワードは6文字以上で入力してください"
+      ]
     }
-};
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+      this.$store.dispatch("userLogin", {
+        email: this.email,
+        password: this.password
+        });
+      }
+    },
+  },
+}
 </script>
 
-<style scoped></style>
+<style>
+
+</style>
