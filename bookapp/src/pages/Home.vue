9e897eec80be8f23fbb5c107dd8d6ebc7ca507fe
OGA <totti0927.oga@gmail.com>
2021-05-24 22:27:40 +0900

    Home.vue修正

--- bookapp/src/pages/Home.vue
+++ bookapp/src/pages/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <span>
-    <v-img src="../assets/書籍MV.png"></v-img>
-  </span>
+  <section class="home">
+    <v-container fluid fill-height class="home">
+      <v-row>
+        <v-col class="home">
+          <h1  class="brown--text darken-4 display-3 font-weight-bold">SHALIBO</h1>
+          <p class="book_discription">知識として定着させよう</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+  
 </template>
 
 <script>
@@ -17,6 +25,15 @@ export default {
 .v-main {
   flex: 0 auto;
   padding: 56px 0 0 0 !important;
+}
+
+.logo_title {
+  text-align: center;
+  font-size: 40px;
+}
+
+.book_discription {
+  text-align: center;
 }
 
 @media screen {
