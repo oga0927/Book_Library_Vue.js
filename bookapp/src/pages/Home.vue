a7e466fdd203b556375584dff5d2aec9c21d9a10
OGA <totti0927.oga@gmail.com>
2021-05-26 22:41:39 +0900

    LP作成

--- bookapp/src/pages/Home.vue
+++ bookapp/src/pages/Home.vue
@@ -1,22 +1,26 @@
 <template>
   <section class="home">
     <v-container fluid fill-height class="home">
-      <v-row>
-        <v-col class="home">
-          <div class="content">
-            <div class="sub_title font-weight-black white--text text-center description">
-              本の知識を自分のものに
-            </div>
-            <!-- <div class="white--text text-center description">
-              本の知識を自分のものに
-            </div> -->
-            <div class="font-weight-black white--text text-center description">
-              「読書管理SHALIBO」
-            </div>
+      <v-layout>
+        <div class="content">
+          <div class="sub_title font-weight-black deep-orange--text text--accent-4 text-center description">
+            本の知識を自分のものに
           </div>
-        </v-col>
-      </v-row>
+          <div class="font-weight-black deep-orange--text text--accent-4 text-center description">
+            「読書管理SHALIBO」
+          </div>
+          <v-btn color="primary" dark to="/bookindex">本を投稿する</v-btn>
+        </div>
+      </v-layout>
     </v-container>
+    <v-main>
+      <div class="discription">
+        <h2>SHALIBOとは</h2>
+        <h4>SHALIBOは読書した内容を記録したり、読んだ本で学んだことを質問に答えてアウトプットできます。<br>
+        本で得た知識を投稿して自分のものにしましょう！
+        </h4>
+      </div>
+    </v-main>
   </section>
   
 </template>
@@ -33,7 +37,7 @@ export default {
 }
 .v-main {
   flex: 0 auto;
-  padding: 56px 0 0 0 !important;
+  padding: 56px 0 250px 0 !important;
 }
 
 .content {
@@ -46,9 +50,20 @@ export default {
 .sub_title {
   font-size: 30px;
 }
-/* 
-.book_discription {
+
+.discription {
+  width: 100%;
+  color: #232323;
+  font-size: 1rem;
+  line-height: 35px;
+  letter-spacing: 0.1rem;
   text-align: center;
+  /* background-color: rgba(219, 197, 152, 0.5) */
+}
+
+/* .discription p {
+  padding: 4px;
+  font-weight: bold;
 } */
 
 @media screen {
