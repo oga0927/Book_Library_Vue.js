61cccac86256d20b46fdd8dc09151a1096b75442
OGA <totti0927.oga@gmail.com>
2021-07-11 13:35:37 +0900

    addBooks修正

--- bookapp/src/App.vue
+++ bookapp/src/App.vue
@@ -20,9 +20,10 @@ import AppFooter from '@/global/AppFooter'
 import firebase from '@/plugins/firebase'
 // ===============================================
 // 修正前 
-// const STORAGE_KEY = 'books'
+// const booksRef = firebase.database().ref('books')
 // 修正後
-const booksRef = firebase.database().ref('books')
+const database = firebase.database();
+const booksRef = 'books';
 // =============================================
 export default {
   name: 'App',
@@ -37,8 +38,10 @@ data(){
   },
 created() {
   // booksへの追加
+  // child_added イベントは通常、データベースからアイテムのリストを取得
   booksRef.on('child_added', (snapshot) => {
     this.books.push(snapshot.val())
+    this.books.id = snapshot.val();
   });
   // booksの変更
   booksRef.on('child_changed', (snapshot) => {
@@ -55,25 +58,52 @@ mounted() {
 methods: {
   addBooks(e) {
     const bookData = {
-    title: e.title,
-    image: e.image,
-    discription: e.discription,
-    readDate: '',
-    memo: '',
-    learn: '',
-    important: '',
-    examples: '',
-    different: '',
-    userId: this.$store.state.userId,
-    }
-    booksRef.push(bookData)
-  
-  // this.saveBooksに保存する
-    this.saveBooks();
-    // 最新に追加したidの取得コード
-    this.goToEditPage()
-    // console.log(this.books.slice(-1)[0].id);
+      title: e.title,
+      image: e.image,
+      discription: e.discription,
+      id: this.books.length,
+      readDate: '',
+      memo: '',
+      learn: '',
+      important: '',
+      examples: '',
+      different: '',
+      userId: this.$store.state.userId,
+      }
+      database.ref(booksRef).push(bookData)
+      .then(() => {
+        console.log('成功');
+      })
+      .catch(() => {
+        console.log('エラー');
+      })
+      
+      
+  // // this.saveBooksに保存する
+  //   this.saveBooks();
+  //   // 最新に追加したidの取得コード
+  //   this.goToEditPage(this.books.id)
+  //   // console.log(this.books.slice(-1)[0].id);
   },
+  // 現状のコード / Realtime Databeseにはオブジェクトで追加されている
+      // ここから
+      // database.ref(BookList).push({
+      //   title: '',
+      //   image: '',
+      // //   // 説明
+      //   discription: '',
+      //   id: this.books.length,
+      //   readDate: '',
+      //   // メモ
+      //   memo: '',
+      //   learn: '',
+      //   important: '',
+      //   examples: '',
+      //   different: '',
+      //   userId: this.$store.state.userId,
+      // })
+      // console.log(value);
+      // ============================================================
     removeBook(x) {
       this.books.splice(x, 1);
       this.saveBooks();
@@ -85,9 +115,9 @@ methods: {
       firebase.database().set(booksRef, parsed)
     },
     // 最新のid
-    goToEditPage() {
+    goToEditPage(id) {
       // ページの切り替え
-      this.$router.push('/edit')
+      this.$router.push(`/edit/${id}`)
     },
   }
 };
