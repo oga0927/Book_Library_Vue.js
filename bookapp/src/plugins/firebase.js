ade173367ad40183b5e9a42ad8294b9843be3349
OGA <otti0927.oga@gmail.com>
2021-09-09 14:57:53 +0900

    test

--- bookapp/src/plugins/firebase.js
+++ bookapp/src/plugins/firebase.js
@@ -3,8 +3,13 @@ import firebase from 'firebase'
 
 
 const firebaseConfig = {
-  
-
+  apiKey: "AIzaSyC9Nxu5MjTts_FnRYnIeqAvNc_Xo3WMtnU",
+  authDomain: "vue-book-library-5d6d5.firebaseapp.com",
+  projectId: "vue-book-library-5d6d5",
+  databaseURL: "https://vue-book-library-5d6d5-default-rtdb.firebaseio.com/",
+  storageBucket: "vue-book-library-5d6d5.appspot.com",
+  messagingSenderId: "376703859489",
+  appId: "1:376703859489:web:61e3d9b3618bcf24a99c60"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
